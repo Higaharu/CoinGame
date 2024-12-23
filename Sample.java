97f2efcb3c33ffa66542b0d790b6255b60d2759b
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2024-12-23 21:33:51 +0900

    SecondCommit

--- Sample.java
+++ Sample.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Main{
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int x = scan.nextInt();
-        int hour = x/3600;
-        int min = (x%3600)/60;
-        int second =(x%3600)%60;
-        System.out.println(hour+":"+min+":"+second);
+        int y = scan.nextInt();
+        int z = scan.nextInt();
+
+        int[] array = {x, y, z};
+
+        Arrays.sort(array);
+
+        for (int num : array) {
+          System.out.print(num + " ");
+      }
     }
 }
