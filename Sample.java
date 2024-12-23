d87a81a22c1c7125e024de398a20b3815fbdef97
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2024-12-23 21:34:38 +0900

    ThirdCommit

--- Sample.java
+++ Sample.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class Main{
-    public static void main(String args[]){
+class Main {
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
+        
+        // 入力を受け取る
         int x = scan.nextInt();
         int y = scan.nextInt();
         int z = scan.nextInt();
 
+        // 配列に格納
         int[] array = {x, y, z};
 
+        // 昇順にソート
         Arrays.sort(array);
 
-        for (int num : array) {
-          System.out.print(num + " ");
-      }
+        // 結果を出力（空白区切りで表示）
+        System.out.println(array[0] + " " + array[1] + " " + array[2]);
     }
 }
